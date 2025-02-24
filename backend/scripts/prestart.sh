2ccbf760d06a6efc6778488e0be4ce1adc0868f5
unnme <unnme@icloud.com>
2025-02-24 15:05:02 +0300

    F

---
+++ backend/scripts/prestart.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+# Let the DB start
+python app/app/backend_pre_start.py
+
+# Change to project root (app/) to ensure alembic.ini is found
+cd "$(dirname "$0")/.." 
+
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+# python app/initial_data.py
